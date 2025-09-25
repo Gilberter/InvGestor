7c053bd1db8514d55361327b876c3e6ffe102b98
Hensemberk <juanchodve14@gmail.com>
2025-09-25 11:43:51 -0500

    Spring Boot initialization

---
+++ Backend/invgestor/src/main/java/com/invgestorback/invgestor/InvgestorApplication.java
@@ -0,0 +1,13 @@
+package com.invgestorback.invgestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvgestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvgestorApplication.class, args);
+	}
+
+}
---
+++ Backend/invgestor/src/test/java/com/invgestorback/invgestor/InvgestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invgestorback.invgestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvgestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
