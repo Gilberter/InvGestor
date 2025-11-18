ed1290cafe105572d0f9a0720eb7022150ccc8aa
Juanse1331 <juansebastian.suarez13@gmail.com>
2025-11-18 14:20:01 -0500

    feat: creacion del boton de configuracion en caso de que sea administrador

--- inv-gestor/src/app/layout/layout.component.ts
+++ inv-gestor/src/app/layout/layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule, MatIconModule],
+  imports: [RouterModule, MatIconModule, MatButtonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
